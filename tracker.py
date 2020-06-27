0dd7535f9ce0ea79da8bcf252b6ba17c6e4fe323
idm2114 <64162393+idm2114@users.noreply.github.com>
2020-06-27 19:31:18 -0400

    fixed issues with printing unnecessary info

--- tracker.py
+++ tracker.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 import time
+import os
 
 trackinglist = []
 
@@ -12,9 +13,14 @@ with open("trackingnumbers.txt", "r") as fileHandle:
         current = line[:-1]
         trackinglist.append(current)
 
-delivered = True
 for number in trackinglist: 
+    # creating boolean flag to track if package was delivered
+    delivered = False
+    
+    #printing tracking number
     print(number)
+
+    #selenium
     with webdriver.Firefox() as driver:
         driver.get("https://www.packagetrackr.com")
         print(driver.title)
@@ -28,20 +34,50 @@ for number in trackinglist:
         #waiting for new page to load
         results = driver.find_elements_by_class_name("media-body")
         for result in results:
-            print(result.text)
-        
-        # trying to remove listed number if item was delivered 
+            # print(result.text)
+       
+        # trying to remove listed number if item was delivered
             if (result.text == "Delivered"):
                 trackinglist.remove(number)
                 delivered = True
 
+        # writing contents to new temp file
+        with open ('tempfile.txt', 'w') as filehandle:
+            for result in results:
+                filehandle.write('%s\n' % result.text)
+
+        # comparing the results of old and new files if old file exists
+        # returning difference to provide most accurate (recent) tracking info
+        try: 
+            oldfile = open((number)+'.txt', "r+")
+            tempfile = open("tempfile.txt", "r+")
+            old_dict = oldfile.readlines()
+            new_dict = tempfile.readlines()
+            oldfile.close()
+            tempfile.close()
+            #finding the difference between the two files
+            diff = [ x for x in new_dict if x not in old_dict ]
+            if diff: 
+                print(diff[0].rstrip())
+                old_dict = new_dict
+                with open ((number)+'.txt', "w") as filehandle:
+                    for line in old_dict:
+                        filehandle.write('%s\n' % line.rstrip())
+        # if older version of the file isn't found, create it based on temp file
+        except:
+            with open((number)+'.txt', "w") as filehandle:
+                for result in results: 
+                    filehandle.write('%s\n' % result.text)
+
     if (delivered):
         print(" ")
         print("package " + number + " was delivered, tracking number removed from list")
-
+        # cleaning out old textfiles for delivered packages
+        os.remove((number)+'.txt')
 #updating tracking list txt file
 with open ('trackingnumbers.txt', 'w') as filehandle:
     for item in trackinglist:
         filehandle.write('%s\n' % item)
 
-
+#removing tempfile 
+os.remove("tempfile.txt")
