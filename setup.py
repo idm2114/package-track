0806f536bb8733a3864e11c95199fc46a555df44
idm2114 <64162393+idm2114@users.noreply.github.com>
2020-07-06 22:35:10 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'package-track',         # How you named your package folder (MyLib)
   packages = ['package-track'],   # Chose the same as "name"
-  version = '0.1.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'command line tool to track packages using gmail api and selenium',   # Give a short description about your library
   long_description = long_description,
@@ -16,22 +16,15 @@ setup(
   author = 'Ian Macleod',                   # Type in your name
   author_email = 'idm2114@columbia.edu',      # Type in your E-Mail
   url = 'https://github.com/idm2114/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/idm2114/package-track/archive/v_02-alpha.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/idm2114/package-track/archive/v_01-2-alpha.tar.gz',    # I explain this later on
   keywords = ['tracking numbers', 'shipping', 'packages'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'pickle',
-          'os',
           'google-api-python-client',
           'google-auth-httplib2',
           'google-auth-oauthlib',
-          'base64',
-          'email',
           'bs4',
-          're',
-          'itertools',
           'selenium',
           'pandas',
-          'csv',
           'pyfiglet',
       ],
   classifiers=[
