2f5ce8f8da5ec4959410c6bed7126d9c2b2860e0
idm2114 <64162393+idm2114@users.noreply.github.com>
2020-07-06 22:11:53 -0400

    fixed version 2-alpha

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
   author = 'Ian Macleod',                   # Type in your name
   author_email = 'idm2114@columbia.edu',      # Type in your E-Mail
   url = 'https://github.com/idm2114/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/idm2114/package-track/archive/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/idm2114/package-track/archive/v_02-alpha.tar.gz',    # I explain this later on
   keywords = ['tracking numbers', 'shipping', 'packages'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pickle',
