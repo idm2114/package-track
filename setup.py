32ae3d4390c458706700175a6fa432e2eb040311
idm2114 <64162393+idm2114@users.noreply.github.com>
2020-07-08 09:55:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'package-track',         # How you named your package folder (MyLib)
   packages = ['packagetrack'],   # Chose the same as "name"
-  version = '0.1.8',      # Start with a small number and increase it with every change you make
+  version = '0.1.9',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'command line tool to track packages using gmail api and selenium',   # Give a short description about your library
   long_description = long_description,
@@ -16,7 +16,7 @@ setup(
   author = 'Ian Macleod',                   # Type in your name
   author_email = 'idm2114@columbia.edu',      # Type in your E-Mail
   url = 'https://github.com/idm2114/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/idm2114/package-track/archive/v_01-8-alpha.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/idm2114/package-track/archive/v_01-9-alpha.tar.gz',    # I explain this later on
   keywords = ['tracking numbers', 'shipping', 'packages'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'google-api-python-client',
